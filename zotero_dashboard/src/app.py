376c8041ceae2882beb4ac4428cadb4f34483b9b
MatthewTee <teelucksingh.matthew1@gmail.com>
2022-09-01 23:03:26 -0400

    base url fix

--- zotero_dashboard/src/app.py
+++ zotero_dashboard/src/app.py
@@ -13,8 +13,7 @@ app = dash.Dash(
     __name__,
     use_pages=True,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
-    suppress_callback_exceptions = True,
-    url_base_pathname="/zotero_dashboard/"
+    suppress_callback_exceptions = True
 )
 server = app.server
 
